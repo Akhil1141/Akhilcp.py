c112f29e763dade47ae748e325cf4b8f88160543
Akhil1141 <165010007+Akhil1141@users.noreply.github.com>
2024-05-04 10:49:02 +0530

    Create map sum pairs.py

---
+++ map sum pairs.py
@@ -0,0 +1,24 @@
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.score=0
+class MapSum:
+    def __init__(self):
+        self.dict1=TrieNode()
+        self.dict2=defaultdict(int)
+    def insert(self, key: str, val: int) -> None:
+        delta=val-self.dict2[key]
+        self.dict2[key]=val
+        dict3=self.dict1
+        for i in key:
+            if i not in dict3.children:
+                dict3.children[i]=TrieNode()
+            dict3=dict3.children[i]
+            dict3.score +=delta
+    def sum(self, prefix: str) -> int:
+        dict3=self.dict1
+        for i in prefix:
+            if i not in dict3.children:
+                return 0
+            dict3=dict3.children[i]
+        return dict3.score
